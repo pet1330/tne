99534556f97ff7a97c99699a587f39d6f5c7f07a
Peter Lightbody <pet1330@gmail.com>
2017-12-22 15:58:58 +0000

    forgotten files

---
+++ app/Country.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Country extends Model
+{
+
+    protected static function boot() {
+        parent::boot();
+
+        static::deleting(function($country) {
+             $country->programmes->each->delete();
+        });
+    }
+
+    protected $table ='countries';
+
+    protected $with = [ ];
+
+    protected $fillable = [ 'name' ];
+
+    protected $hidden = ['deleted_at', 'created_at', 'updated_at'];
+
+    public function programmes()
+    {
+        return $this->hasMany(Programme::class);
+    }
+}
--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,4 +1,13 @@
-require('./bootstrap');
+
+window.axios = require('axios');
+
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+window.axios.defaults.withCredentials = true;
+
+let token = document.head.querySelector('meta[name="csrf-token"]');
+
+window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
 
 window.Vue = require('vue');
 
